ca03594efb127e360a6fe693feba9887dc13a1d1
swapnil1303 <swapnilmakwana4@gmail.com>
2024-01-07 20:52:50 +0530

    worked on score card using js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let direction ={x:0, y:0};
-let speed =2;
+let inputDir ={x:0, y:0};
+let speed =5;
 let lastPaintTime=0;
+let score=0;
 let snakeArr=[
-    {x:13,y:15}
+    {x:5,y:5}
 ]
-food={x:14,y:15}
+let scoreList=document.getElementById('score-list');
+// scoreCard.innerHTML="Right bites :"+ score;
+food={x:10,y:5}
 // Game functions
 function main(ctime){
     window.requestAnimationFrame(main);
@@ -15,7 +18,49 @@ function main(ctime){
     gameEngine();
     // console.log(ctime);
 }
+function isCollide(snakeArr){
+    // bump into self
+    for(let i = 1; i<snakeArr.length;i++){
+        if((snakeArr[0].x===snakeArr[i].x) && (snakeArr[0].y===snakeArr[i].y)){
+            
+            return true;
+        } 
+    }
+    if(snakeArr[0].x>=18 || snakeArr[0].x<=1) return true;
+    if(snakeArr[0].y<=1 || snakeArr[0].y>=18) return true;
+    return false;
+    // bump into walls
+}
 function gameEngine(){
+    if(isCollide(snakeArr)){
+        inputDir={x:0,y:0};
+        alert("Game over.. Press any key to play again");
+        snakeArr=[{x:5,y:5}];
+
+        // create score element 
+        const scoreElement = document.createElement('div');
+        scoreElement.classList.add('scores');
+        scoreElement.innerHTML = score;
+        scoreList.appendChild(scoreElement);
+        // reset score
+        score=0;
+    }
+    // if food is eaten increament score and extend snake body
+    if((snakeArr[0].y===food.y) && (snakeArr[0].x===food.x)){
+        score+=1;
+        // scoreCard.innerHTML="Right bites :"+ score;
+        let a=2;
+        let b=16;
+        snakeArr.unshift({x:snakeArr[0].x + inputDir.x, y:snakeArr[0].y+ inputDir.y});
+        food = {x: Math.round(a+ (b-a)*Math.random()),y: Math.round(a+(b-a)*Math.random())};
+    }
+    // Moving the snake
+    for(let i=snakeArr.length-2;i>=0;i--){
+        // const element=array[i];
+        snakeArr[i+1]={...snakeArr[i]};
+    }
+    snakeArr[0].x+=inputDir.x;
+    snakeArr[0].y+=inputDir.y;
     // part 1 : update snake array
     // part 2: render the snake and food
     // display snake
